e0feb019443b40fa601c892bb3ae2f59ac00d8fe
Vasiliy Storozhev <vvstorozhev@bk.ru>
2024-03-24 22:40:21 +0300

    Initial commit

---
+++ taskmanager/main/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+
+from .forms import TaskForm
+from .models import Task
+
+
+# Create your views here.
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма передана не корректно'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
